1c8342336ed30d69e4b3cd93995064b668528321
matuboxtukuri <161719469+matuboxtukuri@users.noreply.github.com>
2024-08-01 22:42:12 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -2,25 +2,31 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <title>PHP基礎編</title>
 </head>
-
 <body>
     <p>
         <?php
-      $nums = [15, 4, 18, 23, 10];
-      sort($nums);
-      echo "昇順にソートします。<br>";
-      foreach($nums as $num) {        
-      echo $num . '<br>';
-      }
-      $nums = [15, 4, 18, 23, 10];
-      rsort($nums);
-      echo "降順にソートします。<br>";
-      foreach($nums as $num) {        
-      echo $num . '<br>';
-      }
+    $nums = [15, 4, 8, 23, 10];
+    function sort_2way($array, $order) {   
+      if($order)  {
+            echo "昇順にソートしました。<br>";
+            sort($array);
+            foreach($array as $num) {
+                echo $num . '<br>';
+            }
+        }else {
+            echo "降順にソートしました。<br>";
+            rsort($array);
+            foreach($array as $num) {
+                echo $num . '<br>';
+            }
+        }
+    }
+    sort_2way($nums, true);
+    sort_2way($nums, false);    
+        
      ?>
     </p>
 </body>
